af0b5bc7688b828f8e39a200e9b2effa90e48a40
Franciskkang <fjtyas@gmail.com>
2025-07-08 09:46:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log("후츠릿 구독 좋아요");
+//console.log(2 + 2);
+
+//자바스크립트 주석: 슬러시2개
+//변수
+const youtube = "후츠릿"; //상수
+let comment = "구독좋아여";
+
+console.log("youtube", youtube); //''로 문자열을 보여주겠다 console이 너무 많으면 햇갈리니
+console.log("🚀 ~ comment:", comment);
+console.log("🚀 ~ console:", console);
+
+// youtube = "태요미네";
+
+comment = "알림설정";
+console.log("🚀 ~ comment:", comment);
